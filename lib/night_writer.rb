d775e4df5c71a1031df9fe8a239d800dce56e59d
brickstar <brickstar@gmail.com>
2018-03-24 23:59:34 -0600

    added test helper

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,9 @@
+require_relative 'dictionary'
 require 'pry'
+
+# this is a NightWriter class
 class NightWriter
+  include Dictionary
   attr_reader :dictionary,
               :first_row,
               :second_row,
@@ -106,9 +110,9 @@ nw = NightWriter.new
 nw.translate_phrase("Love, Love, Love\n Love, Love, Love\n Love, Love, Love\n
                     There's nothing you can do that can't be done Nothing
                     you can sing that can't be sung Nothing you can say, but you
-                    can learn how to play the game it's easy Nothing you can
-                    make that can't be made No one you can save that can't be
-                    saved Nothing you can do, but you can learn how to be you in
-                    time It's easy All you need is love, all you need is love
-                    All you need is love, love, love is all you needAll you need
-                    is love All you need is love, love, love is all you need")
+                    can learn how to play the game it's easy") #Nothing you can
+                    #make that can't be made No one you can save that can't be
+                    #saved Nothing you can do, but you can learn how to be you in
+                    #time It's easy All you need is love, all you need is love
+                    #All you need is love, love, love is all you needAll you need
+                    #is love All you need is love, love, love is all you need
